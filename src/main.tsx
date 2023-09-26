bc5fbfe8350e15e7e9be9cbfe0953aa3acc159f5
Денис <pro@MacBook-Air.Dlink>
2023-09-26 17:40:50 +0300

    404 bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store, { persistor } from './store/store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter>
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor}>
 					<App />
 				</PersistGate>
 			</Provider>
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 )
